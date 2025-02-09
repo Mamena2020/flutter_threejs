69b81f2d60f64aa3085b5e0eaa5d3553f21888d6
mamena2020 <mamenaware751@gmail.com>
2025-02-09 11:07:29 +0900

    [1]. Readmme-> show demo ss

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_threejs/fbx_viewer.dart';
+// import 'package:test_threejs/fbx_viewer.dart';
 import 'package:test_threejs/threejs_view.dart';
 // import 'package:test_threejs/threejsweb_view.dart';
 // import 'package:test_threejs/webview_windows.dart';
@@ -20,15 +20,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -40,19 +38,19 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Three Js"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (_) => const FbxViewer()));
-              },
-              child: const Text("three_js"),
-            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context)
+            //         .push(MaterialPageRoute(builder: (_) => const FbxViewer()));
+            //   },
+            //   child: const Text("three_js"),
+            // ),
             // ElevatedButton(
             //   onPressed: () {
             //     Navigator.of(context).push(
@@ -72,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.of(context).push(
                     MaterialPageRoute(builder: (_) => const ThreejsView()));
               },
-              child: const Text("three_js Flutter"),
+              child: const Text("Test"),
             ),
           ],
         ),
